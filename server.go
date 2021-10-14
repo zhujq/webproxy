f9c5f002c499348855ccb966f4cf8c27c7d4e76f
zhujq <=>
2021-10-15 00:53:49 +0800

    railway debug

--- server.go
+++ server.go
@@ -112,7 +112,7 @@ func handleConnection(clientConn net.Conn) {
 		if len(resolvedId) > 1 {
             log.Println("success to get resolvedid:" + resolvedId)
 
-			fmt.Fprintf(clientConn, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nContent-Type: application/octet-stream\r\nConnection: keep-alive\r\n\r\n")
+			fmt.Fprintf(clientConn, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nContent-Type: application/octet-stream\r\nConnection: keep-alive\r\nTransfer-Encoding: chunked\r\n\r\n")
 		/*	fmt.Fprintf(clientConn, "Upgrade: websocket\r\n")
 			fmt.Fprintf(clientConn, "Connection: Upgrade\r\n")
 			fmt.Fprintf(clientConn, "Content-Type: application/octet-stream\r\n")
@@ -173,7 +173,7 @@ func handleConnection(clientConn net.Conn) {
 		}
 
 		if len(resolvedId) > 1 {
-			fmt.Fprintf(clientConn, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nContent-Type: application/octet-stream\r\nConnection: keep-alive\r\n\r\n")
+			fmt.Fprintf(clientConn, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nContent-Type: application/octet-stream\r\nConnection: keep-alive\r\nTransfer-Encoding: chunked\r\n\r\n")
         /*    fmt.Fprintf(clientConn, "Upgrade: websocket\r\n")
             fmt.Fprintf(clientConn, "Connection: Upgrade\r\n")
 			fmt.Fprintf(clientConn, "Content-Type: application/octet-stream\r\n")
