33191fd25f31a2f650d20ad2126cfa4dd8dba0c1
zhujq <=>
2021-10-15 10:20:46 +0800

    ssh nopasswd&color

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ mkdir -p /var/run/sshd
 mv /authorized_keys /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 nohup /usr/sbin/sshd -D &
+echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 cd /
 chmod +x server
 /server 
