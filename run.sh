21bd41cb21a51fe22c89880a35b4162f434b2f7b
zhujq <38308874+zhujq@users.noreply.github.com>
2022-04-20 19:25:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 export USER=root
 mkdir -p /var/run/sshd
 mv /authorized_keys /root/.ssh/authorized_keys
+mv /id_rsa /root/.ssh/id_rsa
+mv /id_rsa.pub /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/id_rsa
+chmod 644 /root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/authorized_keys
 nohup /usr/sbin/sshd -D &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
