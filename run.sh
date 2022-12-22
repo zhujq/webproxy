295901bc8668e92d720ad6fd22b8028a12f3454f
zhujq <14065567@qq.com>
2022-12-22 12:04:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,15 @@ chmod 600 /root/.ssh/authorized_keys
 #chmod 644 /root/.ssh/id_rsa.pub
 nohup /usr/sbin/sshd -D &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
+
+mkdir -p /root/tail
+cd /root/tail
+wget https://pkgs.tailscale.com/stable/${TSFILE}
+tar xzf tailscale_1.34.1_amd64.tgz --strip-components=1
+mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+nohup ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=railway-vps
+
 mkdir -p /v2ray
 cd /v2ray
 wget -O v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.23.2/v2ray-linux-64.zip
