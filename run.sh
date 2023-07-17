635d9286e0459c537176909d1f1b0e4ef2ca13c9
zhujq <14065567@qq.com>
2023-07-17 14:25:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -24,7 +24,5 @@ cp -f /config.json .
 chmod +x v2ray
 V2RAY_VMESS_AEAD_FORCED=false nohup ./v2ray run> /dev/null 2>&1 &
 cd /
-ifconfig -a
-echo "127.0.0.1 zeabur" >> /etc/hosts
 chmod +x server
 /server 
